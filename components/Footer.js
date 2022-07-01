6ef8ac09ce869983679bbeff526d6f068769ffe9
Eshad Dewan <eshaddewan571@gmail.com>
2022-07-02 00:09:15 +0600

    kjkkl

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 
 const Footer = () => {
     return (
-        <div className='text-white footer-bg p-0 mt-5'>
+        <div className='text-white bg-secondary p-0 mt-5'>
             <Container className="py-3">
                 <span className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat doloremque ad dolor nulla inventore, minima pariatur sit neque aut, ex quos rerum. Similique praesentium esse laudantium, reprehenderit obcaecati iste eos.</span>
             </Container>
